af194ed69936b8ffb8ba84c2124404f0ece7c877
Larry Kluger <larry.kluger@docusign.com>
2021-04-02 12:06:54 +0300

    OAuth working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,344 @@
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { ToastContainer, toast } from 'react-toastify';
+import OAuthImplicit from './OAuthImplicit';
 
 import './App.css';
 
-
-const App = () => (
-    <Container className="p-3">
-      <Jumbotron>
-        <h1 className="header">Welcome To React-Bootstrap</h1>
-      </Jumbotron>
-    </Container>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            accessToken: undefined,
+            expires: undefined,
+            name: undefined,
+            email: undefined,
+            externalAccountId: undefined,
+            accountName: undefined,
+            accountId: undefined,
+            baseUri: undefined,
+            page: 'welcome', // initial page. Pages: welcome|loggedIn
+            working: false,
+            workingMessage: '',
+            responseErrorMsg: undefined,
+            responseEnvelopeId: undefined,
+            responseAvailableApiRequests: undefined,
+            responseApiRequestsReset: undefined,
+            responseSuccess: undefined,
+            responseTraceId: undefined,
+            formName: '',
+            formEmail: '',  
+        };
+        this.oAuthImplicit = new OAuthImplicit(this);
+        //this.docusign = new DocuSign(this);
+        this.logout = this.logout.bind(this);
+        this.startAuthentication = this.startAuthentication.bind(this);
+        this.formNameChange = this.formNameChange.bind(this);
+        this.formEmailChange = this.formEmailChange.bind(this);
+        this.sendEnvelope = this.sendEnvelope.bind(this);
+    }
   
+    async componentDidMount() {
+        const hash = window.location.hash;
+        if (!hash) {return}
+        // possible OAuth response
+        this.setState({working: true, workingMessage: 'Authenticating'});
+        await this.oAuthImplicit.completeLogin();
+        this.setState({working: false});        
+    }
+  
+    componentWillUnmount() {
+    }
+      
+    startAuthentication() {
+      this.oAuthImplicit.startLogin();
+    }
+    
+    /**
+     * Is the accessToken ok to use?
+     * @returns boolean accessTokenIsGood
+     */
+    checkToken() {
+      if (
+        !this.state.accessToken ||
+        this.state.expires === undefined ||
+        new Date() > this.state.expires
+      ) {
+        // Need new login. Only clear auth, don't clear the state (leave form contents);
+        this.clearAuth();
+        this.setState({ page: 'welcome', working: false });
+        toast.error('Your login session has ended.\nPlease login again', {
+          autoClose: 8000,
+        });
+        return false;
+      }
+      return true;
+    }
+  
+    /**
+     * This method clears this app's authentication information.
+     * But there may still be an active login session cookie
+     * from the IdP. Your IdP may have an API method for clearing
+     * the login session.
+     */
+    logout() {
+      this.clearAuth();
+      this.clearState();
+      this.setState({ page: 'welcome' });
+      toast.success('You have logged out.', { autoClose: 1000 });
+    }
+  
+    /**
+     * Clear authentication-related state
+     */
+    clearAuth() {
+      this.setState({
+        accessToken: undefined,
+        expires: undefined,
+        accountId: undefined,
+        externalAccountId: undefined,
+        accountName: undefined,
+        baseUri: undefined,
+        name: undefined,
+        email: undefined,
+      })
+    }
+  
+    /**
+     * Clear the app's form and related state
+     */
+    clearState() {
+      this.setState({
+        formName: '',
+        formEmail: '',
+        working: false,
+        responseErrorMsg: undefined,
+        responseEnvelopeId: undefined,
+        responseAvailableApiRequests: undefined,
+        responseApiRequestsReset: undefined,
+        responseSuccess: undefined,
+        responseTraceId: undefined,
+      });
+    }
+  
+    /**
+     * Process the oauth results.
+     * This method is called by the OAuthImplicit class
+     * @param results
+     */
+    oAuthResults(results) {
+      this.setState({
+        accessToken: results.accessToken, expires: results.expires, 
+        name: results.name, externalAccountId: results.externalAccountId,
+        email: results.email, accountId: results.accountId,
+        accountName: results.accountName, baseUri: results.baseUri,
+        page: 'loggedIn',
+        formName: results.name, // default: set to logged in user
+        formEmail: results.email,
+      });
+  
+      toast.success(`Welcome ${results.name}, you are now logged in`);
+    }
+  
+    formNameChange(event) {
+      this.setState({ formName: event.target.value });
+    }
+  
+    formEmailChange(event) {
+      this.setState({ formEmail: event.target.value });
+    }
+  
+    async sendEnvelope() {
+      this.setState({
+        responseErrorMsg: undefined,
+        responseEnvelopeId: undefined,
+        responseAvailableApiRequests: undefined,
+        responseApiRequestsReset: undefined,
+        responseSuccess: undefined,
+        responseTraceId: undefined,
+      });
+      if (!this.checkToken()) {
+        return; // Problem! The user needs to login
+      }
+      if (!this.state.formEmail || this.state.formEmail.length < 5) {
+        toast.error("Problem: Enter the signer's email address");
+        return;
+      }
+      if (!this.state.formName || this.state.formName.length < 5) {
+        toast.error("Problem: Enter the signer's name");
+        return;
+      }
+    
+      this.setState({ working: true });
+      const results = await this.docusign.sendEnvelope();
+      const { apiRequestsReset } = results;
+      const responseApiRequestsReset = apiRequestsReset
+        ? new Date(apiRequestsReset) : undefined;
+      this.setState({
+        working: false,
+        responseSuccess: results.success,
+        responseErrorMsg: results.errorMsg,
+        responseEnvelopeId: results.envelopeId,
+        responseAvailableApiRequests: results.availableApiRequests,
+        responseTraceId: results.traceId,
+        responseApiRequestsReset,
+      });
+    }
+  
+    render() {
+        let pagebody;
+        switch (this.state.page) {
+            case 'welcome': // not logged in
+                pagebody = this.Welcome();
+                break;
+            case 'loggedIn':
+                pagebody = this.LoggedIn();
+                break;
+            default:
+                pagebody = this.Welcome();
+        };
+        let nameBlock;
+        if (this.state.accessToken) {
+            nameBlock = (
+                <Navbar.Text>
+                    {this.state.name}<br/>
+                    {this.state.accountName} ({this.state.externalAccountId})
+                <Nav>
+                    <Nav.Link href="#" onClick={()=> this.logout()}>Logout</Nav.Link>
+                </Nav>
+                </Navbar.Text>
+            )
+            } else {
+                nameBlock = null;
+            }
+
+        return (
+            <>
+            <Navbar fixed="top" bg="primary" variant="dark" >
+                <Navbar.Brand>DocuSign Code Example</Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                    {nameBlock}
+                </Navbar.Collapse>
+            </Navbar>
+            <ToastContainer />
+            <Container fluid className='bodyMargin' style={{ display: this.state.working ? 'block' : 'none' }}>
+                <Row className='justify-content-center'>
+                    <div className="spinner" />
+                </Row>
+                <Row className='justify-content-center'>
+                    <h3>{this.state.workingMessage}…</h3>
+                </Row>
+            </Container>
+            {pagebody}
+            </>
+        )
+    }
+  
+    LoggedIn() {
+      const resetTime = this.state.responseApiRequestsReset;
+      const resetTimeString = resetTime
+        ? new Intl.DateTimeFormat('en-US', {
+            dateStyle: 'medium',
+            timeStyle: 'full',
+          }).format(resetTime)
+        : undefined;
+      return (
+        <Container fluid className='bodyMargin'>
+          <div>
+            <h1>Send an Envelope with an embedded signing ceremony</h1>
+            <form>
+              <label>
+                Name:
+                <input
+                  type="text"
+                  value={this.state.formName}
+                  onChange={this.formNameChange}
+                />
+              </label>
+              <label>
+                Email:
+                <input
+                  type="text"
+                  value={this.state.formEmail}
+                  onChange={this.formEmailChange}
+                />
+              </label>
+            </form>
+            <div>
+              <button type="button" onClick={this.sendEnvelope}>
+                Send Envelope
+              </button>
+            </div>
+            <h1>Results</h1>
+            <h1>
+              {this.state.responseSuccess !== undefined ? (
+                this.state.responseSuccess ? (
+                  <>✅ Success!</>
+                ) : (
+                  <>❌ Problem!</>
+                )
+              ) : null}
+            </h1>
+            {this.state.responseErrorMsg ? (
+              <p>Error message: {this.state.responseErrorMsg}</p>
+            ) : null}
+            {this.state.responseEnvelopeId ? (
+              <p>Envelope ID: {this.state.responseEnvelopeId}</p>
+            ) : null}
+            {this.state.responseAvailableApiRequests ? (
+              <p>
+                Available API requests: {this.state.responseAvailableApiRequests}
+              </p>
+            ) : null}
+            {resetTimeString ? (
+              <p>API requests reset time: {resetTimeString}</p>
+            ) : null}
+            {this.state.responseTraceId ? (
+              <p>
+                Trace ID: {this.state.responseTraceId}. Please include with all
+                customer service questions.
+              </p>
+            ) : null}
+          </div>
+          </Container>
+      );
+    }
+  
+    Welcome() {
+        return (
+    <Container fluid className='welcomeMargin'>
+        <Row>
+            <Col>
+                <Jumbotron>
+                    <h1>React Example with OAuth Authentication</h1>
+                    <p>
+                        In this example the user authenticates with DocuSign via the OAuth Implicit grant flow.
+                        Since the app will then have an access token for the user, the app can call any 
+                        DocuSign eSignature REST API method.
+                    </p>
+                    <p>
+                        Use this example for apps used by the staff of your organization who have 
+                        DocuSign accounts. For example, an application could pull data from multiple
+                        sources and then send an envelope that includes the data.
+                    </p>
+                    <p>
+                        Login with your DocuSign Developer (Demo) credentials.
+                    </p>
+                    <p>
+                        <Button variant="primary" onClick={this.startAuthentication}>Login</Button>
+                    </p>
+                </Jumbotron>
+            </Col>
+        </Row>
+    </Container>
+    )
+    }
+}
 export default App;
---
+++ src/OAuthImplicit.js
@@ -0,0 +1,194 @@
+/**
+ * This file's functions are used for OAuthImplicit grant and
+ * related authentication operations.
+ */
+import { toast } from 'react-toastify';
+
+const expirationBuffer = 10 * 60; // 10 minute buffer
+const sdkString = 'codeEg_react';
+const urlFrag = '/restapi/v2.1'; // DocuSign specific
+const log = m => {console.log(m)}
+const oauthState = 'oauthState'; // The name of the localStorage item for the OAuth state parameter
+
+class OAuthImplicit {
+    //
+    // Static methods
+    //
+    /**
+     * Generate a psuedo random string
+     * See https://stackoverflow.com/a/27747377/64904
+     * @param {integer} len  length of the returned string
+     */
+    static generateId(len = 40) {
+        // dec2hex :: Integer -> String i.e. 0-255 -> '00'-'ff'
+        const arr = new Uint8Array((len || 40) / 2);
+        function dec2hex(dec) {
+        return `0${dec.toString(16)}`.substr(-2);
+        }
+        window.crypto.getRandomValues(arr);
+        return Array.from(arr, dec2hex).join('');
+    }
+
+    //
+    // constructor for the class
+    //
+    constructor(app) {
+        this.app = app;
+    }
+
+    /**
+     * Handle incoming OAuth Implicit grant response
+     */
+    async completeLogin() {
+        const config = window.config;
+        const hash = window.location.hash
+            , accessTokenFound = hash && hash.substring(0,14) === '#access_token=';
+        if (!accessTokenFound) {return} // EARLY RETURN
+
+        // Avoiding an injection attack: check that the hash only includes expected characters
+        // An example: #access_token=eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQkAAAABAAUABwCA5xeRWULXSAgAgCc7n5xC10gCAHR-9s2Nn_hNv77uMjz2XvIVAAEAAAAYAAEAAAAFAAAADQAkAAAAN2ZiMmFhYmUtYjI3NC00MDQzLWIyYTItY2M1NDg0MGRmMzJkMACA5xeRWULXSBIAAQAAAAsAAABpbnRlcmFjdGl2ZQ.eUVqrmf4wZ8EQRqNPDI2AumqbNVrxlxCD0uq8YCWNDAvC1lAakTdInXUBLMMsjN-NxXChP7UX8VF870-kSsiR9izXj55-YjTHveCyEdJ-V9iSotZpHLZ58Y2ScGF4mreBmZqFX2jVed43NY71b-qAlghTP7VfMulP0KnpYwLWLllNSB-OIlg68tsVhbhiUayC5fLHRv0mm1lB9LCjf5GzeP3TJLTkQ5NiAqEmEGqdmo4r8BkRxxW4X1hBHq2iD3E2cMugdOpMFamUFRw3vHCHdDiJxYMy6RO8ogtpE98n915hj4jTc2wbtRnJqwiDA90mo-eq4pL5uzw1wKVQLqF6A&expires_in=28800&token_type=bearer&state=e3f287fbe932b904a660282242bfc58bd6a67fe2
+        // No characters other than #.-&=_ a-z A-Z 0-9 (no spaces)
+        const hashRegex = /[^#.\-&=_a-zA-Z0-9]/;
+        if (hash.search(hashRegex) !== -1) {
+            console.error (`Potential XSS attack via fragment (#) value: ${hash}`);
+            toast.error('Potential XSS attack via the fragment value. Please login again.', {
+                autoClose: 7000});
+            return
+        } 
+
+        const oauthStateValue = localStorage.getItem(oauthState);
+        const regex = /(#access_token=)(.*)(&expires_in=)(.*)(&token_type=)(.*)(&state=)(.*)/
+            , results = regex.exec(hash)
+            , accessToken = results[2]
+            , expiresIn = results[4]
+            , incomingState = results[8]
+            , stateOk = incomingState === oauthStateValue
+            ;
+        if (!stateOk) {
+            toast.error('State error during login. Please login again.', {
+                autoClose: 10000});
+            console.error(`OAuth state mismatch!! Expected state: ${oauthStateValue}; received state: ${incomingState}`);
+            return // EARLY RETURN
+        }
+        // hash was good, so erase it from the browser
+        window.history.replaceState(null, '', config.DS_APP_URL);
+
+        // calculate expires
+        let expires = new Date()
+        expires.setTime(expires.getTime() + (expiresIn - expirationBuffer)* 1000)
+        this.accessToken = accessToken;
+
+        const toastId = toast.success('Completing the login process...', {autoClose: 7000});
+
+        // call /oauth/userinfo for general user info
+        // This API method is common for many IdP systems.
+        // But the exact format of the response tends to vary.
+        // The following works for the DocuSign IdP.
+        let userInfoResponse;
+        try {
+            userInfoResponse = await this.fetchUserInfo();
+        } catch (e) {
+            const msg = `Problem while completing login.\nPlease retry.\nError: ${e.toString()}`;
+            log.error(msg);
+            toast.error(msg, { autoClose: 10000 });
+            return;
+        }
+        if (!userInfoResponse || !userInfoResponse.ok) {
+            const msg = `Problem while completing login.\nPlease retry.\nError: ${userInfoResponse.statusText}`;
+            log.error(msg);
+            toast.error(msg, { autoClose: 10000 });
+            return;
+        }
+        const userInfo = await userInfoResponse.json();
+        const defaultAccount = userInfo.accounts.filter((acc) => acc.is_default)[0];
+        if (!defaultAccount) {
+            const msg = `Problem: the user does not have a default account. Contact DocuSign Customer Service to fix.`;
+            log.error(msg);
+            toast.error(msg, { autoClose: 10000 });
+            return;
+        }
+        // 
+        // Need to select the right proxy for the API call
+        let baseUri;
+        if (defaultAccount.base_uri === config.DS_API_CORS_PROXY_FOR) {
+            baseUri = config.DS_API_CORS_PROXY;
+        }
+        if (!baseUri) {
+            const msg = `Problem: no proxy for ${defaultAccount.base_uri}.`;
+            log.error(msg);
+            toast.error(msg, { autoClose: 10000 });
+            return; 
+        }
+            
+        const externalAccountId = await this.getExternalAccountId(
+            defaultAccount.account_id, baseUri);
+        toast.dismiss(toastId);
+        this.app.oAuthResults({
+            accessToken,
+            expires,
+            name: userInfo.name,
+            email: userInfo.email,
+            accountId: defaultAccount.account_id,
+            externalAccountId,
+            accountName: defaultAccount.account_name,
+            baseUri: defaultAccount.base_uri,
+        })
+    }
+
+    /**
+     * Start the login flow by computing the Implicit grant URL
+     * and redirecting to the URL for the user.
+     */
+    startLogin() {
+        const oauthStateValue = OAuthImplicit.generateId();
+        localStorage.setItem(oauthState, oauthStateValue); // store for when we come back
+        let config = window.config;
+        const url =
+        `${config.DS_IDP}/oauth/auth?` +
+        `response_type=token&` +
+        `scope=${config.IMPLICIT_SCOPES}&` +
+        `client_id=${config.DS_CLIENT_ID}&` +
+        `state=${oauthStateValue}&` +
+        `redirect_uri=${config.DS_APP_URL}`;
+
+        window.location = url;
+    }
+
+    /**
+     * A relatively common OAuth API endpoint for obtaining information
+     * on the user associated with the accessToken
+     */
+    async fetchUserInfo() {
+        return fetch(`${window.config.DS_AUTHENTICATION}/oauth/userinfo`, {
+            headers: new Headers({
+                Authorization: `Bearer ${this.accessToken}`,
+                Accept: `application/json`,
+                'X-DocuSign-SDK': sdkString,
+            }),
+        })
+    }
+
+    /**
+     * Fetch the user-friendly version of the accountId.
+     * See https://developers.docusign.com/docs/esign-rest-api/reference/accounts/accounts/get/
+     */
+    async getExternalAccountId(accountId, baseUri) {
+        try {
+            const url = `${baseUri}${urlFrag}/accounts/${accountId}`;
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: new Headers({
+                    Authorization: `Bearer ${this.accessToken}`,
+                    Accept: `application/json`,
+                    'X-DocuSign-SDK': sdkString,
+                })
+            });
+            const data = response && response.ok && (await response.json());
+            return data.externalAccountId;
+        } catch (e) {
+            return null;
+        }
+    }
+}
+
+export default OAuthImplicit;
