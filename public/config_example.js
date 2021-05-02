34df3ba017dcde0761928b45c81d228774189546
Larry Kluger <larry.kluger@docusign.com>
2021-05-02 17:31:44 +0300

    Rel 1

--- public/config_example.js
+++ public/config_example.js
@@ -8,13 +8,14 @@ var config = {};
 // complete as config.js.
 
 // Your app's URL
-config.DS_APP_URL='http://localhost:3000/react-oauth-new-tab-docusign/build';
-// development url default is 'http://localhost:3000/react-oauth-new-tab-docusign/build';
+config.DS_APP_URL='http://localhost:3000/react-oauth-docusign/build';
+// development url default is 'http://localhost:3000/react-oauth-docusign/build';
 
 // If config.DS_REDIRECT_AUTHENTICATION is true, then add a
-// redirect URI to the integration key of DS_APP_URL
+// redirect URI to the integration key of DS_APP_URL since
+// the OAuth flow will restart the application
 //
-// If config.DS_REDIRECT_AUTHENTICATION is true, then add a
+// If config.DS_REDIRECT_AUTHENTICATION is false, then add a
 // redirect URI to the integration key of 
 // DS_APP_URL/oauthResponse.html
 
@@ -25,8 +26,11 @@ config.IMPLICIT_SCOPES='signature';
 config.DS_IDP='https://account-d.docusign.com';
 
 // Your private API CORS proxies
+// Add additional entries for the demo and prod platforms that your 
+// users' accounts need
 config.DS_API_CORS_PROXIES= {
     'https://na2.docusign.net' : 'https://xxxproxy.example.com',
+    'https://na3.docusign.net' : 'https://xxxproxy.example.com',
     'https://demo.docusign.net': 'https://xxxproxy.example.com',
 }
 
